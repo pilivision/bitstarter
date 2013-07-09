a78332918210b5c95bf3744c937133faefe5552a
pilivision <pilivision.studios@gmail.com>
2013-07-09 09:20:36 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -2,11 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var myBuff = new Buffer(128); 
+var myStr = fs.readFileSync('./index.html', 'utf-8');
+
 
 app.get('/', function(request, response) {
-  var fs = require('fs');
-  var myBuff = new Buffer(128); 
-  var myStr = fs.readFileSync('./index.html', 'utf-8');
   Response.send(myStr);
 });
 

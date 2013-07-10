8ee31133ffa2e03f5e6c8cf6efc30c529dd41d60
pilivision <pilivision.studios@gmail.com>
2013-07-10 02:40:37 +0000

    Working grader.js

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://stormy-reaches-9488.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -33,6 +34,7 @@ var assertFileExists = function(infile) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
+
     return instr;
 };
 
@@ -40,17 +42,46 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var cheerioHtmlFile2 = function(htmlfile) {
+    return cheerio.load(htmlfile);
+};
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
+
     $ = cheerioHtmlFile(htmlfile);
+
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
+
+        //console.log(ii);
+        //console.log(checks[ii]);
+        //console.log(present);
+    }
+    return out;
+};
+
+var checkHtmlFile2 = function(htmlfile, checksfile) {
+
+    console.log('Called: checkHtml2');
+
+    $ = cheerioHtmlFile2(htmlfile);
+
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+
+        //console.log(ii);
+        //console.log(checks[ii]);
+        //console.log(present);
     }
     return out;
 };
@@ -65,10 +96,34 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <the_url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url){
+
+      console.log('Selected URL: ');
+      console.log(program.url);
+      var sys = require('util');
+      var rest = require('restler');
+
+      rest.get(program.url).on('complete', function(result) {
+        if (result instanceof Error) {
+         sys.puts('Error: ' + result.message);
+         this.retry(5000); // try again after 5 sec
+        } else {
+          //sys.puts(result);
+          //console.log(result);
+          var checkJson = checkHtmlFile2(result, program.checks);
+          var outJson = JSON.stringify(checkJson, null, 4);    
+          console.log(outJson);
+        }
+      });
+
+    } else{
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
+     
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }

a94ffb6162095a31913e8bda81c4e9390a4ed21c
pilivision <pilivision.studios@gmail.com>
2013-07-18 18:57:54 -0400

    Updated web.js, no need to re-start anymore

--- web.js
+++ web.js
@@ -1,15 +1,14 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
-
 var fs = require('fs');
-var myStr = fs.readFileSync('index.html', 'utf-8');
+var myStr;
 
 app.get('/', function(request, response) {
+  myStr = fs.readFileSync('index.html', 'utf-8');	
   response.send(myStr);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
